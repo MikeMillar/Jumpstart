6143a7e214e923147d933255f06c7c57e59ed263
Mike Millar <39285355+MikeMillar@users.noreply.github.com>
2018-06-24 23:37:46 -0600

    added carousel sliding jumbotron, hiding navbar, and animations to photos

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,10 +1,26 @@
 $(document).ready(function() {
-  $('.single-item').slick();
+  $('.carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
 
+  let scrollLength = 0;
 
-
-
-
+  $(window).scroll(function() {
+    let scrollTop = $(this).scrollTop();
+    if (scrollTop - scrollLength > 50) {
+      let navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      scrollLength = scrollTop;
+    } else if (scrollLength - scrollTop > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      scrollLength = scrollTop;
+    }
+  });
 
 
 
